343185d70bb1aa1afc3314d8567fec8121728635
unknown <quiramapino347@gmail.com>
2020-08-30 14:51:35 -0500

    montandoarchivos de random quote

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Buttons from './Buttons';
+
+export default class Cards extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        quotes: {},
+        curColor: this.props.color,
+        tweetUrl: "https://twitter.com/intent/tweet/?text="
+      }
+      
+      this.getNewQuote = this.getNewQuote.bind(this);
+      // this.handleNewQuote = this.handleNewQuote.bind(this);
+    }
+
+    componentDidMount() {
+      this.getNewQuote();//llama la funcion al inciar la aplicacion para que me traiga todos los datos
+    }
+    
+    getNewQuote(){
+      const app = this;
+      fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      .then((response) => response.json())
+      .then((data) => {
+        app.setState({
+          quotes: data.quotes[Math.floor(Math.random() * data.quotes.length)]
+          // quoteText: data.quotes.quote,quote de la api
+          // quoteAuthor: data.quotes.author,
+        //   tweetUrl: "https://twitter.com/intent/tweet/?text=" + data.content.replace(/ /g, "+")
+        });
+      });
+    }
+    
+    render() {
+        return (
+            <div className="card">
+                  <h2>{this.state.quotes.quote}</h2>
+                  <p>{this.state.quotes.author}</p>
+                  <Buttons changecolor={this.props.changecolor} valor={this.getNewQuote}/>
+              </div>
+        )
+    }
+}
